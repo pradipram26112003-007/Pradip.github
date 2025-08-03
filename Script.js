ffde022e7fd253d92946731cb26e07aea08a8d1e
pradipram26112003-007 <pradipram26112003@gmail.com>
2025-08-03 13:53:18 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,21 @@
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function deleteLast() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    const display = document.getElementById("display");
+    display.value = eval(display.value);
+  } catch (e) {
+    document.getElementById("display").value = "Error";
+  }
+}
